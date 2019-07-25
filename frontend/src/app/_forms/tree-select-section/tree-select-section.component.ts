a4faf9da4ac7be1829a3e38632a8453c03f0e389
eleanor <eleanorcjkgraham@gmail.com>
2019-07-25 16:42:51 -0400

    WIP a few steps closer with the checkboxes functioning roughly as expected. Still not properly toggling, saving, etc.

--- frontend/src/app/_forms/tree-select-section/tree-select-section.component.ts
+++ frontend/src/app/_forms/tree-select-section/tree-select-section.component.ts
@@ -1,7 +1,6 @@
 import { SelectionModel } from '@angular/cdk/collections';
 import { NestedTreeControl } from '@angular/cdk/tree';
 import { Component, OnInit } from '@angular/core';
-import { of as observableOf } from 'rxjs';
 import { FieldType } from '@ngx-formly/material';
 import { Category } from '../../_models/category';
 
@@ -11,12 +10,14 @@ import { Category } from '../../_models/category';
   styleUrls: ['./tree-select-section.component.scss']
 })
 export class TreeSelectSectionComponent extends FieldType implements OnInit {
-  treeControl = new NestedTreeControl<Category>(node => observableOf(node.children));
+  treeControl = new NestedTreeControl<Category>(node => node.children);
 
   nodes = {};
 
   /** The selection for checklist */
   checklistSelection = new SelectionModel<Category>(true /* multiple */);
+
+  getLevel = (node: Category) => node.level;
 
   ngOnInit() {
   }
@@ -51,9 +52,63 @@ export class TreeSelectSectionComponent extends FieldType implements OnInit {
     this.checklistSelection.isSelected(node)
       ? this.checklistSelection.select(...descendants)
       : this.checklistSelection.deselect(...descendants);
+
+    // Force update for the parent
+    descendants.every(child =>
+      this.checklistSelection.isSelected(child)
+    );
+    this.checkAllParentsSelection(node);
   }
 
-  toggleExpand($event, node: Category): void {
-    $event.preventDefault();
-}
+
+  /* Checks all the parents when a leaf node is selected/unselected */
+  checkAllParentsSelection(node: Category): void {
+    let parent: Category | null = this.getParentNode(node);
+    while (parent) {
+      this.checkRootNodeSelection(parent);
+      parent = this.getParentNode(parent);
+    }
+  }
+
+    /** Check root node checked state and change it accordingly */
+  checkRootNodeSelection(node: Category): void {
+    const nodeSelected = this.checklistSelection.isSelected(node);
+    const descendants = this.treeControl.getDescendants(node);
+    const descAllSelected = descendants.every(child =>
+      this.checklistSelection.isSelected(child)
+    );
+    if (nodeSelected && !descAllSelected) {
+      this.checklistSelection.deselect(node);
+    } else if (!nodeSelected && descAllSelected) {
+      this.checklistSelection.select(node);
+    }
+  }
+
+  /* Get the parent node of a node */
+  getParentNode(node: Category): Category | null {
+    const currentLevel = this.getLevel(node);
+
+    if (currentLevel < 1) {
+      return null;
+    }
+
+    const startIndex = this.treeControl.dataNodes.indexOf(node) - 1;
+
+    for (let i = startIndex; i >= 0; i--) {
+      const currentNode = this.treeControl.dataNodes[i];
+
+      if (this.getLevel(currentNode) < currentLevel) {
+        return currentNode;
+      }
+    }
+    return null;
+  }
+
+  /** Select the category so we can insert the new item. */
+  addNewItem(node: Category) {
+    // const parentNode = this.flatNodeMap.get(node);
+    // this.database.insertItem(parentNode!, '');
+    this.treeControl.expand(node);
+  }
+
 }
