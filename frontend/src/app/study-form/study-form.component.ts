6dbc792fae8d0c2297ed22f624397cb08c89acc4
eleanor <eleanorcjkgraham@gmail.com>
2019-12-30 15:08:37 -0500

    WIP study form is pre-populating all non-select fields successfully. Submit is still disabled.

--- frontend/src/app/study-form/study-form.component.ts
+++ frontend/src/app/study-form/study-form.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../_services/api/api.service';
 import { Study } from '../_models/study';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Organization } from '../_models/organization';
 import { Investigator } from '../_models/investigator';
+import { Category } from '../_models/category';
 
 
 enum PageState {
@@ -26,7 +27,8 @@ export class StudyFormComponent implements OnInit {
 
   model: any = {};
   updatedModel: any = {};
-  studyForm: FormGroup;
+
+  category: Category;
 
   statusOptions = [
       {'value': 'currently_enrolling', 'label': 'Currently Enrolling'},
@@ -43,8 +45,27 @@ export class StudyFormComponent implements OnInit {
     ];
   orgOptions: Organization[];
   investOptions: Investigator[];
+  categoryOptions: Category[];
 
   createNew = false;
+
+  studyForm = this.formBuilder.group({
+    title: ['', Validators.required],
+    short_title: ['', Validators.required],
+    description: ['', Validators.required],
+    short_description: ['', Validators.required],
+    participant_description: ['', Validators.required],
+    benefit_description: ['', Validators.required],
+    investigators: [''],
+    organization: [''],
+    location: [''],
+    status: ['', Validators.required],
+    coordinator_email: [''],
+    eligibility_url: [''],
+    image_url: [''],
+    categories: [''],
+    ages: [''],
+  });
 
   constructor(
     private api: ApiService,
@@ -54,10 +75,10 @@ export class StudyFormComponent implements OnInit {
   ) {
     this.getOrganizations();
     this.getInvestigators();
+    this.getCategories();
   }
 
   ngOnInit() {
-    // this.model.createNew = false;
     this.loadData();
   }
 
@@ -75,6 +96,13 @@ export class StudyFormComponent implements OnInit {
     )
   }
 
+  getCategories() {
+    this.api.getCategories().subscribe( cats => {
+       return this.categoryOptions = cats;
+      }
+    )
+  }
+
   loadData() {
     this.route.params.subscribe(params => {
 
@@ -85,8 +113,7 @@ export class StudyFormComponent implements OnInit {
         this.api.getStudy(studyId).subscribe(study => {
           this.study = study as Study;
           this.model = this.study;
-          this.loadStudyInvestigators(study);
-          this.loadStudyCategories(study, () => this.loadForm());
+          this.loadForm();
         });
       } else {
         this.createNew = true;
@@ -98,81 +125,39 @@ export class StudyFormComponent implements OnInit {
     });
   }
 
-  loadStudyCategories(study: Study, callback: Function) {
-    this.model.categories = [];
-    if (study.study_categories.length > 0) {
-      for (const cat in study.study_categories) {
-        this.model.categories[study.study_categories[cat].category.id] = true;
-        callback();
-      }
-    } else {
-      callback();
-    }
-  }
-
-  loadStudyInvestigators(study: Study) {
-    this.model.investigators = [];
-    if (study.study_investigators.length > 0) {
-      for (const i in study.study_investigators) {
-        this.model.investigators.push(study.study_investigators[i].investigator);
-      }
-    }
-  }
-
   loadForm() {
-    this.studyForm = this.formBuilder.group({
-      title: [this.study.title, Validators.required],
-      short_title: [this.study.short_title, Validators.required],
-      description: [this.study.description, Validators.required],
-      short_description: [this.study.short_description, Validators.required],
-      participant_description: [this.study.participant_description, Validators.required],
-      benefit_description: [this.study.benefit_description, Validators.required],
-      investigators: [this.study.investigators],
-      organization: [this.study.organization],
-      location: [this.study.location],
-      status: [this.study.status, Validators.required],
-      coordinator_email: [this.study.coordinator_email],
-      eligibility_url: [this.study.eligibility_url],
-      image_url: [this.study.image_url],
-      categories: [this.study.categories],
-      ages: [this.study.ages],
+    this.studyForm.patchValue({
+      title: this.study.title,
+      short_title: this.study.short_title,
+      description: this.study.description,
+      short_description: this.study.short_description,
+      participant_description: this.study.participant_description,
+      benefit_description: this.study.benefit_description,
+      investigators: this.study.investigators,
+      organization: this.study.organization,
+      location: this.study.location,
+      status: this.statusOptions.find(opt => opt.value === this.study.status),
+      coordinator_email: this.study.coordinator_email,
+      eligibility_url: this.study.eligibility_url,
+      image_url: this.study.image_url,
+      categories: this.getStudyCategories(),
+      ages: this.study.ages,
     });
     this.state = this.pageState.SHOW_FORM;
+
+    console.log('this.studyForm', this.studyForm);
   }
 
-  updateRelatedModel(model_name, model_name_plural) {
-    const join_name = 'study_' + model_name_plural;
-    const api_name = 'Study' + model_name.charAt(0).toUpperCase() + model_name.slice(1);
-    const model_id = model_name + '_id';
-    const relModIds = this.study[join_name].map(relMod => relMod[model_name].id);
-    // Add the new related models
-    for (const relMod in this.model[model_name_plural]) {
-      if (!relModIds.includes(Number(relMod))) {
-        this.api[`add${api_name}`]({"study_id": this.study.id, model_id: Number(relMod)}).subscribe();
-      }
-    }
-    // Remove any deleted related models
-    for (const relMod in this.study[join_name]) {
-      if (!this.model[model_name_plural][this.study[join_name][relMod][model_id]]) {
-        this.api[`delete${api_name}`](this.study[join_name][relMod]).subscribe();
-      }
-    }
+  compareObjects(o1: any, o2: any): boolean {
+    return o1 && o2 ? o1.id === o2.id : o1 === o2;
   }
 
-  updateStudyCategories() {
-    const scIds = this.study.study_categories.map(sc => sc.category.id);
-    // Add the new categories
-    for (const cat in this.model.categories) {
-      if (!scIds.includes(Number(cat))) {
-        this.api.addStudyCategory({study_id: this.study.id, category_id: Number(cat)}).subscribe();
-      }
+  getStudyCategories() {
+    let categories = [];
+    for (let i in this.study.study_categories) {
+      categories.push(this.study.study_categories[i].category)
     }
-    // Remove any deleted categories
-    for (const sc in this.study.study_categories) {
-      if (!this.model.categories[this.study.study_categories[sc].category_id]) {
-        this.api.deleteStudyCategory(this.study.study_categories[sc]).subscribe();
-      }
-    }
+    return categories;
   }
 
   addStudyCategories(study_id) {
@@ -181,59 +166,8 @@ export class StudyFormComponent implements OnInit {
     }
   }
 
-  updateStudyInvestigators() {
-    const siIds = this.study.study_investigators.map(si => si.investigator.id);
-    // Add the new investigators
-    for (const invest in this.model.investigators) {
-      if (!siIds.includes(Number(invest))) {
-        this.api.addStudyInvestigator({study_id: this.study.id, investigator_id: Number(invest)}).subscribe();
-      }
-    }
-    // Remove any deleted investigators
-    for (const si in this.study.study_investigators) {
-      if (!this.model.investigators[this.study.study_investigators[si].investigator_id]) {
-        this.api.deleteStudyInvestigator(this.study.study_investigators[si]).subscribe();
-      }
-    }
-  }
-
-  updateOrganization(callback: Function) {
-    // If the user selects an existing Organization name from the list, it will be saved as an Organization object. If they write in their
-    // own Organization name, it will be saved as a new organization with that name. When saving a new organization, we also create an
-    // updated model so that we don't accidentally save the old version before it's updated. When there is no Organization being saved, all
-    // we do is create the updated model.
-    if (this.model.organization){
-      if (this.model.organization.constructor.name == "String") {
-        this.api.addOrganization({name: this.model.organization}).subscribe( org => {
-          this.model.organization_id = org.id;
-          this.model.organization = org;
-          this.updatedModel = this.model;
-          callback();
-        })
-      } else {
-        this.model.organization_id = this.model.organization.id;
-        this.updatedModel = this.model;
-        callback();
-      }
-    } else {
-      this.updatedModel = this.model;
-      callback();
-    }
-  }
-
   submit() {
     console.log('here is the form on submit', this.studyForm);
-    // Post to the study endpoint, and then close
-    // if (this.form.valid) {
-    //   if (this.createNew) {
-    //     this.updateOrganization(() => this.addAndClose());
-    //   } else {
-    //     // this.updateStudyCategories();
-    //     this.updateRelatedModel('category', 'categories');
-    //     this.updateRelatedModel('investigator', 'investigators');
-    //     this.updateOrganization(() => this.updateAndClose());
-    //   }
-    // }
   }
 
   addAndClose() {
