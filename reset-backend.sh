764ee1d900c3897bc0b8779cb8aaa892ef9de3ac
Aaron Louie <ajlouie@gmail.com>
2020-01-22 09:07:33 -0500

    Replaces venv and requirements.txt with pipenv

--- reset-backend.sh
+++ reset-backend.sh
@@ -8,8 +8,7 @@ BACKEND_PATH="${HOME_DIR}/backend"
 
 echo -e '\n\n*** Resetting backend app... ***\n\n'
 cd $BACKEND_PATH
-source python-env/bin/activate
 export FLASK_APP=./app/__init__.py
-pip3 install -r requirements.txt
+pipenv install
 flask db upgrade
 flask reset
